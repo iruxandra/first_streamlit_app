1cbbe989c52e8f8558e462e41db563b22ea5ed1f
Ruxandra <63659685+iruxandra@users.noreply.github.com>
2022-07-19 17:48:20 +0300

    Update streamlit_app.py
    Created a fruit dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭Build Your On Fruit Smoothie🥝🍇')
 
-
+import pandas as pd
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
