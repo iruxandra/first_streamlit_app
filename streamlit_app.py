5cf7aee38b8dcc8b269e9e9cca957d6a4b364733
Ruxandra <63659685+iruxandra@users.noreply.github.com>
2022-07-19 17:06:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,4 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+streamlit.header('🍌🥭Build Your On Fruit Smoothie🥝🍇')
